78340d2978c824831f75f1d3bc528213983f1f54
Muchael123 <mainamichael2849@gmail.com>
2024-11-04 15:00:31 +0300

    Deploy to genezio

--- Api/src/index.ts
+++ Api/src/index.ts
@@ -1,6 +1,7 @@
 import express, {json, urlencoded} from 'express';
 import productsRoutes from './routes/products/index.js'
 import authRoutes from './routes/auth/index.js'
+  import serverless from "serverless-http";
 
 const app = express();
 const port = 3000;
@@ -14,7 +15,10 @@ app.get('/', (req, res) => {
 
 app.use('/products', productsRoutes);
 app.use('/auth', authRoutes)
-    
-app.listen(port, () => {
-    console.log('App Running on port', port)
-});
+  
+if (process.env.NODE_ENV === "dev") {
+    app.listen(port, () => {
+        console.log('App Running on port', port)
+    });
+}
+  export const handler = serverless(app);
